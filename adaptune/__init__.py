ef86768a07b95ce7fe6a33a2c5b256b8e5f793f2
Miebori Kazuma <km.isetan@gmail.com>
2019-08-31 14:14:26 +0900

    Add submod "rasplay"

--- adaptune/__init__.py
+++ adaptune/__init__.py
@@ -4,4 +4,4 @@ from adaptune import *
 from adaptune.config import *
 from adaptune.monitor import *
 
-__all__ = ['core', 'passalsa', 'monitor', 'dump_defaults']
+__all__ = ['core', 'passalsa', 'monitor', 'dump_defaults', 'dev']
