6406e2806effc56a30c51b960f818ca4fccc2f6b
borley1211 <km.isetan@gmail.com>
2019-09-16 18:21:33 +0900

    UPDATE: 新バージョンのモックアップを制作

--- adaptune/preferences.py
+++ adaptune/preferences.py
@@ -5,7 +5,7 @@ from collections import OrderedDict, defaultdict
 import pathlib
 from asyncio import get_event_loop, create_task, ensure_future, Task
 from asyncio import sleep as asleep
-from typing import Dict, List, Tuple, Union, DefaultDict
+from typing import Dict, List, Tuple, Union, DefaultDict, Coroutine
 import commentjson
 
 from prompt_toolkit import print_formatted_text, prompt
@@ -35,6 +35,7 @@ PathStr = str
 
 
 class PreferenceApp(object):
+    all_done = False
 
     breadcrumb = ""
     inquirer_style = style_from_pygments_dict(
@@ -47,25 +48,29 @@ class PreferenceApp(object):
             Token.Breadcrumb: "#eeeeee",
         }
     )
-    
+
     confdict: Dict = {}
-    attributes: Dict = {}
+
     attr_header: List = []
-    
     root: List[KeyType] = []
+
+    attributes: Dict = {}
     nodes: List[KeyType] = []
+
+    current_node: KeyType
 
     tasks: List[Task] = []
     eventdict: DefaultDict[str, List[Task]] = defaultdict(list)
 
     def __init__(self):
-        pass
+        self.load_config()
+        self.set_header()
 
     def load_config(self):
         self.confdict, _choices = self._get_conf("config.json")
         self.attr_header, self.root = self._get_conf(
             ".attributes.json")
-    
+
     def set_header(self):
         self.attributes = self.attr_header
         self.nodes = self.root
@@ -119,3 +124,145 @@ class PreferenceApp(object):
             config = _eval_values(config)
 
         return config, keylist
+
+    def get_prompt_tokens(self):
+        tokens = []
+        tokens.append((Token.QuestionMark, "?"))
+        tokens.append((Token.Question, " Configure about: "))
+
+        if self.ic.answered:
+            selected = self.ic.get_selection()
+
+            if self.breadcrumb:
+                self.breadcrumb += " >"
+                tokens.append((Token.Breadcrumb, self.breadcrumb))
+            tokens.append((Token.Answer, " " + str(selected)))
+            tokens.append((Token, "\n"))
+            self.breadcrumb += " " + str(selected)
+            self.select_item(selected, self.attributes)
+            self.invalidate()
+        else:
+            if self.breadcrumb:
+                tokens.append((Token.Breadcrumb, self.breadcrumb))
+            tokens.append((Token.Instruction, " (Use arrow keys)"))
+
+        return [("class:" + pygments_token_to_classname(x[0]),
+                 str(x[1])) for x in tokens]
+
+    def select_item(self, key, attributes):
+
+        def get_ic_child(key, attributes):
+            attr_child = attributes[key]
+
+            if isinstance(attr_child, dict):
+                self.attributes, self.nodes = (
+                    attr_child, list(attr_child.keys()))
+            elif isinstance(attr_child, list):
+                self.attributes = self.branches = attr_child
+
+        def _work_before_node(key, attr):
+            nonlocal self
+
+            if isinstance(attr, dict):
+                # if "this branch has only nodes":
+                attr_child = attr[key]
+                if set(attr_child) >= {'type', 'section'}:
+                    notice = "Enter {}".format(attr_child["type"])
+                    notice += " as {}".format(attr_child["section"])
+
+                    def _validfunc(inputs):
+                        nonlocal attr_child
+                        if not attr_child["section"](inputs):
+                            raise ValidationError(
+                                message="This input does not meet the requirements as {}".format(
+                                    attr["section"])
+                            )
+                        elif not isinstance(inputs, attr_child["type"]):
+                            raise ValidationError(
+                                message="This input does not contain the type as {}".format(
+                                    attr["type"])
+                            )
+                        else:
+                            return inputs
+
+                    notice += ":"
+
+                    self.current_node = key
+                    self.attributes = []
+
+                    self.reset()
+                    res = None
+
+                # else(=="this branch has some branches"):
+                # special case in "device"
+                elif key in ["main", "monitor", "input"]:
+                    snddevs = list(sd.query_devices())
+                    devlist = [devinfo["name"] for devinfo in snddevs]
+                    self.current_node = key
+                    self.attributes = []
+
+                    res = False
+
+                elif str(self.current_node) in ["main", "monitor", "input"]:
+                    res = key
+
+                # other cases
+                else:
+                    self.ic = get_ic_child(key, attr)
+                    self.current_node = key
+                    self._set_layout()
+                    res = False
+
+            elif isinstance(attr, list):
+                res = key
+
+            return res
+
+        def _setval_if_key_is_node(key_prev, selected):
+            nonlocal self
+
+            # hw_params
+            if key_prev in ["rate", "periodsize", "channels"]:
+                self.confdict["hw_params"][key_prev] = selected
+            elif key_prev in ["alsa", "sounddevice"]:
+                self.confdict["hw_params"]["formatname"] = selected
+
+            # filter_params
+            elif key_prev in ["mu", "w"]:
+                self.confdict["filter_params"][key_prev] = selected
+
+            # (as below)
+            elif key_prev in ["filter_domain", "filter_algo"]:
+                self.confdict[key_prev] = selected
+
+            # devices
+            elif key_prev in ["main", "monitor", "input"]:
+                self.confdict["devices"][key_prev] = selected
+
+            # [post process]
+            self.current_node = str(selected)
+            self.finished = True
+
+        result = _work_before_node(key, attributes)
+        if result:
+            key_prev = self.current_node
+            _setval_if_key_is_node(key_prev, result)
+
+    def _create_selection_event(self):
+        pass
+
+    def _create_input_event(self):
+        pass
+
+    def _create_print_event(self):
+        pass
+
+    async def _event_loop(
+            self, task: Task, interval: Union[int, float] = 1):
+        pass
+
+    async def async_run(self, coro: Coroutine):
+        pass
+
+    def main(self):
+        pass
