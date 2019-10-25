569845067eaa0861da20d390f67af5d933ffbd9f
borley1211 <km.isetan@gmail.com>
2019-10-25 13:40:18 +0900

    FIX

--- adaptune/sound.py
+++ adaptune/sound.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from typing import Iterator, Optional, Tuple
 
@@ -9,99 +8,11 @@ import sounddevice as sd
 import adaptune.core as core
 from adaptune._load_config import default_filter, dev, domain, hw_params
 
+
 sd.default.samplerate = hw_params["rate"]
 sd.default.dtype = (hw_params["formatname"], hw_params["formatname"])
 sd.default.channels = (hw_params["channels"], hw_params["channels"])
 sd.default.prime_output_buffers_using_stream_callback = True
-
-
-class Source(sd.InputStream):
-    frames: int
-    is_running: bool = False
-
-    def __init__(
-        self,
-        frames=hw_params["frames"],
-        samplerate=None,
-        blocksize=None,
-        device=None,
-        channels=None,
-        dtype=None,
-        latency=None,
-        extra_settings=None,
-        callback=None,
-        finished_callback=None,
-        clip_off=None,
-        dither_off=None,
-        never_drop_input=None,
-        prime_output_buffers_using_stream_callback=None,
-    ):
-        self.frames = frames
-        self.is_running = True
-        super().__init__(
-            samplerate=samplerate,
-            blocksize=blocksize,
-            device=device,
-            channels=channels,
-            dtype=dtype,
-            latency=latency,
-            extra_settings=extra_settings,
-            callback=callback,
-            finished_callback=finished_callback,
-            clip_off=clip_off,
-            dither_off=dither_off,
-            never_drop_input=never_drop_input,
-            prime_output_buffers_using_stream_callback=prime_output_buffers_using_stream_callback,
-        )
-
-    def read_as_iterable(
-        self, frames: Optional[int] = None
-    ) -> Iterator[Tuple[np.ndarray, bool]]:
-        if frames:
-            pass
-        else:
-            frames = self.frames
-        while self.is_running:
-            _readed = self.read(frames)
-            yield _readed
-
-    def stop_iteration(self):
-        self.is_running = False
-        self.stop()
-
-
-class Sink(sd.OutputStream):
-    def __init__(
-        self,
-        samplerate=None,
-        blocksize=None,
-        device=None,
-        channels=None,
-        dtype=None,
-        latency=None,
-        extra_settings=None,
-        callback=None,
-        finished_callback=None,
-        clip_off=None,
-        dither_off=None,
-        never_drop_input=None,
-        prime_output_buffers_using_stream_callback=None,
-    ):
-        super().__init__(
-            samplerate=samplerate,
-            blocksize=blocksize,
-            device=device,
-            channels=channels,
-            dtype=dtype,
-            latency=latency,
-            extra_settings=extra_settings,
-            callback=callback,
-            finished_callback=finished_callback,
-            clip_off=clip_off,
-            dither_off=dither_off,
-            never_drop_input=never_drop_input,
-            prime_output_buffers_using_stream_callback=prime_output_buffers_using_stream_callback,
-        )
 
 
 class IterableStream(sd.Stream):
