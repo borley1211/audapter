1abfb0f2bf16e48b023fa1223d5ec60c90e0a65e
borley1211 <km.isetan@gmail.com>
2019-11-13 14:44:42 +0900

    FIX: adopt macrolib

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 
-project = "adaptune"
+project = "audapter"
 copyright = "2019, Miebori Kazuma"
 author = "Miebori Kazuma"
 
