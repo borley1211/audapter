90eef769f58731af8806d3a3c2e375fd25489f4b
borley1211 <km.isetan@gmail.com>
2019-11-14 00:01:47 +0900

    FIX: adopt microlib

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
+import os
+import pip
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from six import iteritems
+
+
+def install_microlibs(sources: dict, is_devmode: bool):
+    print("installing microlibs in {} mode".format("development" if is_devmode else "normal"))
+    
+    for k, v in iteritems(sources):
+        pass
+
+
 setup()
