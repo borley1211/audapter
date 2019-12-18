e39884dfc5c98955ea33707a4f8380a387ef1f19
borley1211 <km.isetan@gmail.com>
2019-12-18 17:54:58 +0900

    NEW: helper/filter.py

--- src/audapter/helper/filter.py
+++ src/audapter/helper/filter.py
@@ -5,4 +5,3 @@ def gen_input_from_history(desired : np.ndarray, n : int):
     d_arr = np.array(desired, dtype="float64")
     x_arr = np.array([d_arr[i:i+n] for i in range(len(d_arr)-n+1)])
     return x_arr
-
